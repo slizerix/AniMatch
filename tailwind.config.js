710c14e027ffb57ccdb7c3e0fe00131d826fb647
slizerix <165804415+slizerix@users.noreply.github.com>
2025-03-30 08:41:59 +0300

    it works

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        animation: {
+          'fade-in': 'fadeIn 0.5s ease-out',
+        },
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: 0 },
+            '100%': { opacity: 1 },
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
